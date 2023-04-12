f8e2d09a0d26642639ffa8cd3efba14c9c7f6f1f
Michał Bujacz <micha26404@gmail.com>
2023-04-12 20:37:28 +0200

    Mode selection on admin panel changes content of mode info element

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WPF.database;
 using static System.Net.Mime.MediaTypeNames;
+using Microsoft.Win32;
+using System.IO;
 
 namespace WPF
 {
@@ -32,7 +34,7 @@ namespace WPF
 			Clients_Click(sender, e);
 			Orders_Click(sender, e);
 		}
-		public void query(DataGrid grid, String query) 
+		public void query(DataGrid grid, string query) 
 		{
 			string strConnection = Properties.Settings.Default.WPF_DBConnectionString;
 			SqlConnection con = new SqlConnection(strConnection);
@@ -46,6 +48,21 @@ namespace WPF
 			DataTable dtRecord = new DataTable();
 			sqlDataAdap.Fill(dtRecord);
 			grid.ItemsSource = dtRecord.DefaultView;
+		}
+		public string query(string query)
+		{
+			string strConnection = Properties.Settings.Default.WPF_DBConnectionString;
+			SqlConnection con = new SqlConnection(strConnection);
+
+			SqlCommand sqlCmd = new SqlCommand();
+			sqlCmd.Connection = con;
+			sqlCmd.CommandType = CommandType.Text;
+			sqlCmd.CommandText = query;
+			SqlDataAdapter sqlDataAdap = new SqlDataAdapter(sqlCmd);
+
+			DataTable dtRecord = new DataTable();
+			sqlDataAdap.Fill(dtRecord);
+			return dtRecord.ToString();
 		}
 		private void Movies_Click(object sender, RoutedEventArgs e)
 		{
@@ -71,21 +88,27 @@ namespace WPF
 		{
 			query(OrdersCatalog, "Select * from orders");
 		}
+		public int getmovie_id() {
+			//get selected item id from selected row
+			DataRowView rowview = MoviesCatalog.SelectedItem as DataRowView;
+			return int.Parse(rowview.Row["id"].ToString());
+		}
 		//Trailer panel
-		public double Volume { get; set; }
-		private void MediaOpened(object sender, RoutedEventArgs e)
+
+		//action on trailer load
+		private void TrailerInit(object sender, RoutedEventArgs e)
 		{
 			//seek slider init
-			SeekSlider.Maximum = Trailer.NaturalDuration.TimeSpan.TotalMilliseconds;
+			//SeekSlider.Maximum = Trailer.NaturalDuration.TimeSpan.TotalMilliseconds;
 		}
 		private void AdjustVolume(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
-			Trailer.Volume = (double)VolumeSlider.Value;
+			//Trailer.Volume = (double)VolumeSlider.Value;
 		}
 		private void PlayTrailer(object sender, RoutedEventArgs e)
 		{
 			Trailer.Play();
-			Trailer.Volume = (double)VolumeSlider.Value;
+			//Trailer.Volume = (double)VolumeSlider.Value;
 		}
 		private void PauseTrailer(object sender, MouseButtonEventArgs e)
 		{
@@ -99,50 +122,79 @@ namespace WPF
 		{
 			int SliderValue = (int)SeekSlider.Value;
 
-			// Overloaded constructor takes the arguments days, hours, minutes, seconds, milliseconds.
-			// Create a TimeSpan with miliseconds equal to the slider value.
 			TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue);
 			Trailer.Position = ts;
 		}
-		private void TrailerFile(object sender, RoutedEventArgs e)//Trailer file path
+		//Load trailer from database file path
+		private void LoadTrailer(object sender, RoutedEventArgs e)
 		{
-
+			//get selected item id from selected row
+			DataRowView rowview = MoviesCatalog.SelectedItem as DataRowView;
+			string id = rowview.Row["id"].ToString();
+			
+			//Set trailer source from query to uri
+			string query_result = query("select trailer_path from movies where id=" + id);
+			if (query_result != null) 
+			{
+				string file= query_result;
+				//Trailer file path correctness check
+				if (File.Exists(file))
+				{
+					Uri uri = new Uri(query_result);
+					Trailer.Source = uri;
+				}
+				else
+				{ 
+					MessageBox.Show("Trailer file not found. Use set trailer function to select trailer file");
+					//Remove bad file path from database
+					DeleteTrailer(null, null);
+				}
+			} 
+			else MessageBox.Show("Trailer file path not set. Trailer_path is null.");
 		}
-		private void SetTrailer(object sender, MouseButtonEventArgs e)//Set trailer file path
+		//Set trailer file path and update database
+		private void SetTrailer(object sender, MouseButtonEventArgs e)
 		{
-
+			OpenFileDialog op = new OpenFileDialog();
+			op.Title = "Select trailer";
+			if (op.ShowDialog() == true)
+			{
+				Uri uri = new Uri(op.FileName);
+				Trailer.Source = uri;
+			}
+			//TODO update database
 		}
 		private void DeleteTrailer(object sender, MouseButtonEventArgs e)
 		{
-
+			Trailer.Source = null;
+			//TODO update database
+			query("update movies set trailer_path=null where id="+getmovie_id());
 		}
 		//Poster panel
 		private void RemovePoster(object sender, RoutedEventArgs e)
 		{
-
-        }
-		private void ChangePoster(object sender, RoutedEventArgs e)
-		{
-
-		}
-		private void AddPoster(object sender, RoutedEventArgs e)
-		{
-
+			Poster.Source = null;
+			//TODO update database
 		}
 		private void SetPoster(object sender, MouseButtonEventArgs e)
 		{
-
-		}
-		private void DeletePoster(object sender, MouseButtonEventArgs e)
-		{
-
+			OpenFileDialog op = new OpenFileDialog();
+			op.Title = "Select poster";
+			op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+			  "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+			  "Portable Network Graphic (*.png)|*.png";
+			if (op.ShowDialog() == true)
+			{
+				Poster.Source = new BitmapImage(new Uri(op.FileName));
+			}
+			//TODO update database
 		}
 		//Admin panel
-		private void EditMode(object sender, MouseButtonEventArgs e)
+		private void SubmitAdd(object sender, MouseButtonEventArgs e)
 		{
 
         }
-		private void AddMode(object sender, MouseButtonEventArgs e)
+		private void SubmitEdit(object sender, MouseButtonEventArgs e)
 		{
 
 		}
@@ -200,7 +252,6 @@ namespace WPF
 		{
 
 		}
-
 		private void FilterFirstName_Click(object sender, MouseButtonEventArgs e)
 		{
 
@@ -213,30 +264,33 @@ namespace WPF
 		{
 
 		}
+		//Filter orders
+		private void FilterOrderMovie_Click(object sender, MouseButtonEventArgs e)
+		{
+
+		}
 		private void FilterLName_Click(object sender, MouseButtonEventArgs e)
 		{
 
 		}
-		private void OrderItem_return(object sender, RoutedEventArgs e)
+
+		private void FilterFName_Click(object sender, MouseButtonEventArgs e)
 		{
 
 		}
-		private void OrderItem_edit(object sender, RoutedEventArgs e)
+		private void FilterRentDate_Click(object sender, MouseButtonEventArgs e)
 		{
 
 		}
-		private void ClientItem_add(object sender, RoutedEventArgs e)
+		private void FilterDueDate_Click(object sender, MouseButtonEventArgs e)
 		{
 
 		}
-		private void ClientItem_edit(object sender, RoutedEventArgs e)
+		private void FilterReturnDate_Click(object sender, MouseButtonEventArgs e)
 		{
 
 		}
-		private void OrderItem_add(object sender, RoutedEventArgs e)
-		{
-
-		}
+		//Movies right click menu
 		private void MovieItem_rent(object sender, RoutedEventArgs e)
 		{
 
@@ -259,12 +313,66 @@ namespace WPF
 		}
 		private void MovieItem_trailer(object sender, RoutedEventArgs e)
 		{
+			TrailerInit(null, null);
+		}
+		//Clients right click menu
+		private void ClientItem_add(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void ClientItem_edit(object sender, RoutedEventArgs e)
+		{
+
+		}
+		//Orders right click menu
+		private void OrderItem_return(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void OrderItem_edit(object sender, RoutedEventArgs e)
+		{
+
+		}
+		private void OrderItem_add(object sender, RoutedEventArgs e)
+		{
 
 		}
 		private void OrderItem_delete(object sender, RoutedEventArgs e)
 		{
 
 		}
-		
+		//Mode selectors
+		//add or edit mode; false is add, true is edit.
+		bool mode = false;
+		private void SelectAddMode(object sender, MouseButtonEventArgs e)
+		{
+			mode = false;
+			ModeSelected.Text = "add\nmode";
+			ModeSelected.Foreground = Brushes.White;
+			//set radial background
+			RadialGradientBrush radialGradient = new RadialGradientBrush();
+
+			radialGradient.GradientStops.Add(new GradientStop(Colors.Green, 0.8));
+			radialGradient.GradientStops.Add(new GradientStop(Colors.Transparent, 1.0));
+
+			radialGradient.Freeze();
+
+			ModeSelected.Background = radialGradient;
+		}
+		private void SelectEditMode(object sender, MouseButtonEventArgs e)
+		{
+			mode = true;
+			ModeSelected.Text = "edit\nmode";
+			ModeSelected.Foreground = Brushes.White;
+			//set radial background
+			RadialGradientBrush radialGradient = new RadialGradientBrush();
+
+			radialGradient.GradientStops.Add(new GradientStop(Colors.Red, 0.8));
+			radialGradient.GradientStops.Add(new GradientStop(Colors.Transparent, 1.0));
+
+			radialGradient.Freeze();
+
+			ModeSelected.Background = radialGradient;
+		}
 	}
 }
